3111e7c7582f7444981305dec16f29eb6d9a588e
Quentin de Longraye <quentin@dldl.fr>
2018-06-23 15:28:10 +0200

    adds mongo database connection

--- server/README.md
+++ server/README.md
@@ -1,1 +1,9 @@
 # Server side
+
+The servers require the following environment variables to be set (possibly using a `.env` file):
+
+```sh
+NODE_ENV=development
+MONGO_URL=<Url to MongoDB>
+MONGO_DB_NAME=<MongoDB database name>
+```
---
+++ server/src/database.js
@@ -0,0 +1,21 @@
+const MongoClient = require('mongodb').MongoClient
+
+let connection = null
+
+function getConnection () {
+  if (connection) {
+    return connection
+  }
+
+  return new Promise ((resolve, reject) => {
+    MongoClient.connect(process.env.MONGO_URL, (err, client) => {
+      if (err) {
+        return reject(err)
+      }
+
+      connection = client.db(process.env.MONGO_DB_NAME)
+
+      return resolve(connection)
+    })
+  })
+}
